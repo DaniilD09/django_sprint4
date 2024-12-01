fb7b9c3eb18faf5802dafc12e94e5a64db60095d
DaniilD09 <dan80iil@gmail.com>
2024-12-01 23:06:25 +0300

    blogicum 5.0

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -7,11 +7,19 @@ handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
 
 auth_urls = [
-    path('registration/', CreateView.as_view(
-        template_name='registration/registration_form.html',
-        form_class=UserCreationForm,
-        success_url=reverse_lazy('blog:index'),
-    ), name='registration'),
+    path(
+        'registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
+    path(
+        '',
+        include('django.contrib.auth.urls')
+    ),
 ]
 
 urlpatterns = [
@@ -29,17 +37,5 @@ urlpatterns = [
         'admin/',
         admin.site.urls
     ),
-    path(
-        'auth/',
-        include('django.contrib.auth.urls')
-    ),
-    path(
-        'auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
-        ),
-        name='registration'
-    ),
+    path('auth/', include(auth_urls)),
 ]
