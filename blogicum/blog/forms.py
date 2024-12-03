bd11c5af970ce8ab0f250cb32b77dd555e3fdbad
DaniilD09 <dan80iil@gmail.com>
2024-12-03 07:57:26 +0300

    blogicum 7.3

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 
 from .models import Comment, Post
--- blogicum/blog/mixins.py
+++ blogicum/blog/mixins.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth import login
 from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
 from django.urls import reverse
